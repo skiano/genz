4428187f4c1dbdb38b75fae16811cfebae16807e
Greg Skiano <gregoryskiano@gmail.com>
2022-02-08 15:46:46 -0800

    holy crap it traverses sooo much better

---
+++ src/mappy.mjs
@@ -0,0 +1,32 @@
+function* each(arr) {
+  let i;
+  for (i = 0; i < arr.length; i++) yield arr[i];
+}
+
+function* traverse (arr) {
+  let queue = [each(arr)];
+  while (queue.length) {
+    const { value, done } = queue[0].next();
+    if (Array.isArray(value)) {
+      queue.unshift(each(value));
+    } else {
+      if (value) yield value;
+      if (done) {
+        queue.shift();
+      }
+    }
+  }
+}
+
+const it = traverse([
+  1,
+  2,
+  [3, 4, [5], [6, 7]],
+  8,
+  [[]],
+  9
+])
+
+for (let v of it) {
+  console.log(v);
+}
