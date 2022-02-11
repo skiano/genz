00a8a66a8741768aedf5376831c3e6f4348d0b23
Greg Skiano <gregoryskiano@gmail.com>
2022-02-11 01:24:25 -0800

    maybe better who knows

--- src/tag-primitive.mjs
+++ src/tag-primitive.mjs
@@ -1,4 +1,4 @@
-
+import { TAG_NAMES, VOID_ELEMENTS } from "./constants.mjs";
 
 const each = (arr, i = 0) => {
   const marker = { done: false };
@@ -20,9 +20,7 @@ function traverse (arr, i) {
     if (Array.isArray(value)) { 
       queue.unshift(each(value));
       return next();
-    }
-    
-    else {
+    } else {
       if ((value ?? false) !== false) return value;
       if (done) {
         queue.shift();
@@ -32,25 +30,53 @@ function traverse (arr, i) {
   }
 }
 
-function tag () {
-  const hasAttributes = (
-    typeof arguments[0] == 'object' &&
-    arguments[0] !== null &&
-    typeof arguments[0] !== 'function'
-  );
-  const n = traverse(arguments, hasAttributes ? 1 : 0);
-  console.log(n());
-  console.log(n());
-  console.log(n());
-  console.log(n());
-  console.log(n());
-};
+function createTag (name) {
+  return function tag () {
+    const hasAttributes = (
+      typeof arguments[0] == 'object' &&
+      arguments[0] !== null &&
+      typeof arguments[0] !== 'function'
+    );
 
-tag(
+    let str;
+    let opening = true;
+    let children = traverse(arguments, hasAttributes ? 1 : 0);
+
+    return function next() {
+      if (opening) {
+        str = name = 'html' ? `<${name}` : `<${name}`;
+        if (hasAttributes) {
+          for (let a in arguments[0]) {
+            str += ` ${a}="${arguments[0][a]}"`
+          }
+        }
+        opening = false;
+        return str += '>';
+      }
+      str = children();
+      if (typeof str === 'undefined') {
+        str = VOID_ELEMENTS[name] ? '/>' : `</${name}>`;
+      }
+      return str;
+    }
+  };
+}
+
+export const _ = TAG_NAMES.reduce((o, name) => {
+  o[name] = createTag(name, { isVoid: VOID_ELEMENTS[name] });
+  return o;
+}, {});
+
+
+const n = _.div(
   1,
   2,
   [3, 4, 5],
   3,
   4
-)
+);
+
+console.log(n())
+console.log(n())
+console.log(n())
 
