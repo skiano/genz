4e61d23f2ac94bb3bd7c25b218ca2ad36ffa4f71
Greg Skiano <gregoryskiano@gmail.com>
2022-02-19 15:19:57 -0800

    neato

--- genz.mjs
+++ genz.mjs
@@ -1,6 +1,6 @@
 import { TAG_NAMES, VOID_ELEMENTS } from "./constants.mjs";
 
-function createTag (name) {
+export function createTag (name) {
   return function tag (a0) {
     // 1. If attibutes are passed
     let a;
@@ -29,7 +29,52 @@ function createTag (name) {
   };
 }
 
-export default TAG_NAMES.reduce((o, name) => {
+export const _ = TAG_NAMES.reduce((o, name) => {
   o[name] = createTag(name, { isVoid: VOID_ELEMENTS[name] });
   return o;
 }, {});
+
+export function traverse (arr) {
+  let queue_a = [arr];
+  let queue_i = [0];
+
+  let i;
+  let value;
+  return function next() {
+    if (!(queue_a && queue_a.length)) {
+      queue_a = undefined;
+      queue_i = undefined;
+      return;
+    }
+
+    i = queue_i[0];
+    value = queue_a[0][i];
+    queue_i[0] = i + 1;
+
+    if (typeof value === 'object' && typeof value.length !== 'undefined') { 
+      queue_a.unshift(value);
+      queue_i.unshift(0);
+      return next();
+    } else {
+      if ((value ?? false) !== false) {
+
+        // pass back any promises...
+        if (value.then) { // TODO: add tests for weird promise-looking things...
+          return value;
+        }
+
+        // OUPUT A CHILD STRING!!!!!!
+        return typeof value === 'string'
+          ? value
+          : String(value);
+
+      } else if (queue_i[0] >= queue_a[0].length) {
+        queue_a.shift();
+        queue_i.shift();
+        return next();
+      } else {
+        return next();
+      }
+    }
+  }
+}
--- test/sync.mjs
+++ test/sync.mjs
@@ -1,5 +1,16 @@
-import _ from '../genz.mjs';
+import { _, traverse } from '../genz.mjs';
 import assert from 'assert';
+
+const syncStringify = (it) => {
+  let o;
+  let frags = [];
+  do {
+    o = it();
+    frags.push(o);
+  } while (o);
+
+  return frags.join('');
+}
 
 export default [
 
@@ -24,81 +35,9 @@ export default [
       ])
     );
 
-    // function* each(arr) {
-    //   let i;
-    //   for (i = 0; i < arr.length; i++) yield arr[i];
-    // }
+    const txt = syncStringify(traverse(content));
 
-    // function* traverse (arr) {
-    //   let queue = [each(arr)];
-    //   while (queue.length) {
-    //     const { value, done } = queue[0].next();
-
-    //     if (typeof value === 'object' && typeof value.length !== 'undefined') { 
-    //       queue.unshift(each(value))
-    //     } else {
-    //       if (value) yield value;
-    //       if (done) {
-    //         queue.shift();
-    //       }
-    //     }
-    //   }
-    // }
-
-    const traverse = (arr) => {
-      const queue_a = [arr];
-      const queue_i = [0];
-
-      let i;
-      let value;
-      return function next() {
-        i = queue_i[0];
-        value = queue_a[0][i];
-        queue_i[0] = i + 1;
-
-        if (typeof value === 'object' && typeof value.length !== 'undefined') { 
-          queue_a.unshift(value);
-          queue_i.unshift(0);
-          return next();
-        } else {
-          if ((value ?? false) !== false) {
-
-            // pass back any promises...
-            if (value.then) { // TODO: add tests for weird promise-looking things...
-              return value;
-            }
-
-            // OUPUT A CHILD STRING!!!!!!
-            return typeof value === 'string'
-              ? value
-              : String(value);
-
-          } else if (queue_i[0] >= queue_a[0].length) {
-            queue_a.shift();
-            queue_i.shift();
-            return next();
-          } else {
-            return next();
-          }
-        }
-
-      }
-    }
-
-    const next = traverse(content)
-
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-    console.log(next());
-
+    console.log(txt);
   },
 
 ];
