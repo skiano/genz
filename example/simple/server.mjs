26b086a7138f52fd4bcc346f7e06f41c733faca9
Greg Skiano <gregoryskiano@gmail.com>
2022-02-20 20:25:00 -0800

    with bootstrap

--- example/simple/server.mjs
+++ example/simple/server.mjs
@@ -10,17 +10,17 @@ const ARTICLES = {
   'hello-world': {
     title: 'Hello World',
     body: [
-      { elm: 'p', value: 'This is the first paragraph' },
-      { elm: 'p', value: 'This is the second paragraph' },
-      
+      { elm: 'p', value: 'Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.' },
+      { elm: 'p', value: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.' },
     ]
   },
   'a-very-great-article': {
     title: 'A Very Great Article',
     body: [
-      { elm: 'p', value: 'This is the first paragraph' },
+      { elm: 'p', value: 'Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.' },
       { elm: 'img', attr: { width: '200px', src: 'http://skiano.com/static/art-stew-01.jpg' } },
-      { elm: 'p', value: 'This is the second paragraph' },
+      { elm: 'p', value: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.' },
+      { elm: 'p', value: 'Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'},
     ]
   }
 };
@@ -40,9 +40,21 @@ const Page = ({ title }, content) => {
   return _.html(
     _.head(
       _.title(title),
+      _.meta({
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+      }),
+      _.link({
+        rel: 'stylesheet',
+        href: 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css',
+      }),
+      _.link({
+        rel: 'stylesheet',
+        href: 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css',
+      })
     ),
     _.body(
-      _.main({ class: 'page-wrap' }, content)
+      _.main({ class: 'container' }, content)
     )
   )
 };
@@ -63,9 +75,7 @@ const HomePage = Page({ title: 'Home '}, [
   }
 ]);
 
-const ArticleBody = body => body.map(({ elm, value, attr }) => {
-  return _[elm](attr, value);
-});
+const ArticleBody = body => body.map(({ elm, value, attr }) => _[elm](attr, value));
 
 const ArticlePage = async (ctx) => {
   const article = await fetchArticle(ctx.route.params.article_id);
