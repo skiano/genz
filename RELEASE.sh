5f40fd5f632f402d457ffba2c7ebc0573c2b7bfe
Greg Skiano <gregoryskiano@gmail.com>
2022-02-21 11:39:59 -0800

    hmm

--- RELEASE.sh
+++ RELEASE.sh
@@ -10,6 +10,19 @@ node test/_.mjs;
 echo -e "2. BUILDING PROD FILE"
 node_modules/.bin/rollup src/genz.mjs --plugin terser --file genz.build.mjs
 
-echo -e "\n2. BUMPING VERSION & PUSHING TAG\n"
-npm version $1
-git push origin --tags
+echo -e "\n2. BUILDING PLAYGROUND\n"
+BASE=/genz/ node_modules/.bin/vite build playground --config playground/vite.config.js
+
+echo -e "\n2. DEPLOYING PLAYGROUND TO GITHUB PAGES \n"
+git add playground/dist -f;
+# the head...
+git commit -am "temporarily adding playground"
+git subtree push --prefix playground/dist origin gh-pages
+# Reset the head...
+git reset HEAD~
+rm -rf playground/dist
+git status
+
+# echo -e "\n2. BUMPING VERSION & PUSHING TAG\n"
+# npm version $1
+# git push origin --tags
