f89b63a9886ebfe8395e4abcadd2550e73d5d14c
Greg Skiano <gregoryskiano@gmail.com>
2022-02-21 11:55:05 -0800

    tweak

--- RELEASE.sh
+++ RELEASE.sh
@@ -17,8 +17,7 @@ echo -e "\n2. DEPLOYING PLAYGROUND TO GITHUB PAGES \n"
 git checkout -b temp-deploy
 git add playground/dist -f;
 git commit -am "temporarily adding playground"
-git subtree pull --prefix playground/dist origin gh-pages
-git subtree push --prefix playground/dist origin gh-pages
+git subtree push --prefix playground/dist origin gh-pages --squash
 # Reset the head...
 rm -rf playground/dist
 git checkout main
